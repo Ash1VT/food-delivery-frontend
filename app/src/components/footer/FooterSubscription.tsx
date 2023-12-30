b08ebc282feda4bf05878bc3015fa1f296d5eea5
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-12-30 20:27:23 +0300

    added Footer components

---
+++ app/src/components/footer/FooterSubscription.tsx
@@ -0,0 +1,24 @@
+import { MouseEvent } from 'react'
+import EmailInput from '../inputs/EmailInput'
+import SubscribeButton from '../buttons/SubscribeButton'
+import { FooterSubscriptionProps } from '../../types/footer.type'
+
+import '../../styles/footer_subscription.css'
+
+const FooterSubscription = ({title} : FooterSubscriptionProps) => {
+    const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+        console.log('Click!');
+    }
+
+    return (
+        <div className="footer__subscription__container">
+            <div className="footer__subscription__title">{title}</div>
+            <div className="footer__subscription__wrapper">
+                <EmailInput placeholder='Enter Your Email' className='footer__subscription__email__input'/>
+                <SubscribeButton onClick={onClick}/>
+            </div>
+        </div>
+    )
+}
+
+export default FooterSubscription
