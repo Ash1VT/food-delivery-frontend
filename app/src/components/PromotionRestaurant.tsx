0f036429eecdee2196b85be15f2c2f628cea1149
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-12-30 18:49:25 +0300

    added PromotionRestaurant

---
+++ app/src/components/PromotionRestaurant.tsx
@@ -0,0 +1,33 @@
+import { PromotionElementProps } from '../types/promotion.type';
+import '../styles/promotion_restaurant.css'
+
+
+const PromotionRestaurant = (restaurant_promotion: PromotionElementProps) => {
+    return (
+        <div className="promotion__container">
+            <div style={{
+                backgroundImage: `url(${restaurant_promotion.image_url})`,
+            }} className="promotion__image__wrapper">
+                <div className="promotion__discount__wrapper">
+                    <div className="promotion__discount__amount">
+                        {restaurant_promotion.discount_amount}
+                    </div>
+                    <div className="promotion__discount__percentage__wrapper">
+                        <div className="promotion__discount__percent">
+                            %
+                        </div>
+                        <div className="promotion__discount__text">
+                            Off
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <div className="promotion__restaurant__wrapper">
+                <div className="promotion__restaurant__name">{restaurant_promotion.restaurant_name}</div>
+                <div className="promotion__restaurant__days">{restaurant_promotion.promotion_days_remaining} Days Remaining</div>
+            </div>       
+        </div>
+    )
+}
+
+export default PromotionRestaurant
