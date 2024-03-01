5c7d27e69a6acbbe9af4d853ba27accb700d5a9d
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-03-01 15:53:53 +0300

    changed imports

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,25 @@
 import './App.css';
-import DeliveryAction from './components/delivery/DeliveryAction';
-import DeliveryProcessSection from './sections/DeliveryProcessSection';
-import HeaderSection from './sections/HeaderSection'
-import NavbarSection from './sections/NavbarSection';
-import PromotionsSection from './sections/PromotionsSection';
+import DeliveryAction from './pages/main/delivery-process/delivery-action/DeliveryAction';
+import DeliveryProcessSection from './pages/main/delivery-process/DeliveryProcessSection';
+import HeaderSection from './pages/main/header/HeaderSection'
+import Navbar from './components/navbar/Navbar';
+import PromotionsSection from './pages/main/promotions/PromotionsSection';
 import { PromotionElementProps } from './types/promotion.type';
-import PushableButton from './components/buttons/PushableButton';
+import PushableButton from './components/ui/buttons/PushableButton/PushableButton';
 import { MouseEvent } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
-import PopularItemsSection from './sections/PopularItemsSection';
+import PopularItemsSection from './pages/main/popular-items/PopularItemsSection';
 import { PopularItemProps } from './types/item.type';
-import FeaturedRestaurantsSection from './sections/FeaturedRestaurantsSection';
+import FeaturedRestaurantsSection from './pages/main/featured-restaurants/FeaturedRestaurantsSection';
 import { FeaturedRestaurantProps } from './types/restaurant.type';
-import FoodCategoriesSection from './sections/FoodCategoriesSection';
+import FoodCategoriesSection from './pages/main/food-categories/FoodCategoriesSection';
 import { FoodCategoryProps } from './types/food_category.type';
-import FeaturesSection from './sections/FeaturesSection';
-import BestDealsSection from './sections/BestDealsSection';
+import FeaturesSection from './pages/main/features/FeaturesSection';
+import BestDealsSection from './pages/main/best-deals/BestDealsSection';
 import { BestDealImageLocation, BestDealProps } from './types/best_deal.type';
-import FooterSection from './components/footer';
+import Footer from './components/footer';
 import { FooterMenuColumnProps } from './components/footer/footer.types';
-import MainPage from './pages/MainPage';
+import MainPage from './pages/main/MainPage';
 
 const App = () => {
 
@@ -66,11 +66,11 @@ const App = () => {
     return (
         <div className="container">
             <div>
-                <NavbarSection/>
+                <Navbar/>
                 <HeaderSection/>
             </div>
             <MainPage/>
-            <FooterSection cities={cities} cities_per_column={5} menu_columns={menu_columns}/>
+            <Footer cities={cities} cities_per_column={5} menu_columns={menu_columns}/>
         </div>
     );
 }
