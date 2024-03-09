67324889b250d2072163e3a74fa292a999872e8e
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-03-09 18:13:58 +0300

    added formik in personal information component

--- app/src/pages/profile/personal-information/PersonalInformation.tsx
+++ app/src/pages/profile/personal-information/PersonalInformation.tsx
@@ -1,33 +1,96 @@
-import React from 'react'
-import ControlledDateInput from 'src/components/ui/controlled-date-input/ControlledDateInput'
-import ControlledPhoneInput from 'src/components/ui/controlled-phone-input/ControlledPhoneInput'
-import ControlledTextInput from 'src/components/ui/controlled-text-input/ControlledTextInput'
+import * as Yup from 'yup';
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
+import ControlledInput from 'src/components/ui/controlled-input/ControlledInput';
+import formatDate from 'src/utils/formatDate';
+
+interface FormValues {
+    firstName: string;
+    lastName: string;
+    birthDate: Date;
+    phone: string;
+}
 
 const PersonalInformation = () => {
 
-    const onFirstNameSave = async (firstName: string) => {
-        console.log(`saved firstname ${firstName}`)
+    const initialValues: FormValues = {
+        firstName: "John",
+        lastName: "Doe",
+        birthDate: new Date(),
+        phone: "+375298830141"
     }
 
-    const onLastNameSave = async (lastName: string) => {
-        console.log(`saved lastname ${lastName}`)
-    }
+    const validationSchema: Yup.Schema<FormValues> = Yup.object().shape({
+        firstName: Yup.string().required('First Name is required'),
+        lastName: Yup.string().required('Last Name is required'),
+        birthDate: Yup.date().typeError("Date is not correct").required('Birth Date is required'),
+        phone: Yup.string().required('Phone is required'),
+    })
 
-    const onBirthDaySave = async (birthDate: Date) => {
-        console.log(`saved birthday ${birthDate}`)
+    const handleSubmit = async (values: FormValues, { setSubmitting } : FormikHelpers<FormValues>) => {
+        // Handle form submission here
+        // console.log('Form submitted with values:', values);
+        alert(JSON.stringify(values, null, 2))
+        setSubmitting(false)
     }
-
-    const onPhoneSave = async (phone: string) => {
-        console.log(`saved birthday ${phone}`)
-    }
-
     return (
-        <>
-            <ControlledTextInput label='First Name' value='John' onSave={onFirstNameSave}/>
-            <ControlledTextInput label='Last Name' value='Doe' onSave={onLastNameSave}/>
-            <ControlledDateInput label='Birth Date' value={new Date()} onSave={onBirthDaySave}/>
-            <ControlledPhoneInput label='Phone' value='+375298830141' onSave={onPhoneSave}/>
-        </>
+        <Formik 
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+        {({ errors, values, setFieldValue, submitForm }) => {
+            return (
+                <Form>
+                    <ControlledInput label='First Name' 
+                                    value={values.firstName}
+                                    error={errors.firstName}
+                                    setValue={firstName => { setFieldValue('firstName', firstName) }}
+                                    parseValue={value => value}
+                                    convertToString={value => value}
+                                    onSave={async () => await submitForm()}>
+                        <input type='text'/>   
+                    </ControlledInput>
+ 
+                    <ErrorMessage name='firstName' component='div' />
+
+                    <ControlledInput label='Last Name' 
+                                    value={values.lastName}
+                                    error={errors.lastName}
+                                    setValue={lastName => { setFieldValue('lastName', lastName) }}
+                                    parseValue={value => value}
+                                    convertToString={value => value}
+                                    onSave={async () => await submitForm()}>
+                        <input type='text'/>   
+                    </ControlledInput>
+                        
+                    <ErrorMessage name='lastName' component='div' />
+
+                    <ControlledInput label='Birth Date' 
+                                    value={values.birthDate}
+                                    error={errors.birthDate ? String(errors.birthDate) : undefined}
+                                    setValue={birthDate => { console.log(birthDate); setFieldValue('birthDate', birthDate) }}
+                                    parseValue={value => new Date(value)}
+                                    convertToString={value => formatDate(value)}
+                                    onSave={async () => await submitForm()}>
+                        <input type='date'/>
+                    </ControlledInput>
+
+                    <ErrorMessage name='birthDate' component='div' />
+
+                    <ControlledInput label='Phone' 
+                                    value={values.phone}
+                                    error={errors.phone}
+                                    setValue={phone => { setFieldValue('phone', phone) }}
+                                    parseValue={value => value}
+                                    convertToString={value => value}
+                                    onSave={async () => await submitForm()}>
+                        <input type='tel'/>   
+                    </ControlledInput>
+
+                    <ErrorMessage name='phone' component='div' />
+                </Form>
+            )}}
+        </Formik>
     )
 }
 
