3ff1d6ee097dfaecf3efe6afefeb50b526dad020
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-13 20:03:53 +0300

    removed order cart total price context

--- app/src/components/order-cart/contexts/OrderCartTotalPriceContext.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { OrderCartTotalPriceContextProps } from "../order_cart.types";
-
-export const OrderCartTotalPriceContext = React.createContext<OrderCartTotalPriceContextProps>({
-    totalPrice: 0,
-    setTotalPrice: () => {},
-});
