ea832464525cb8c6d1d92b87207d8dd4be2ebc5b
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-13 19:58:28 +0300

    renamed refs categories

--- app/src/pages/menu/contexts/MenuCategoriesRefsContext.ts
+++ app/src/pages/menu/contexts/MenuCategoriesRefsContext.ts
@@ -2,8 +2,8 @@ import React from 'react'
 import { MenuCategoriesRefsContextProps } from '../menu_page.types'
 
 const MenuCategoriesRefsContext = React.createContext<MenuCategoriesRefsContextProps>({
-    categories: [],
-    setCategories: () => {}
+    categoriesRefs: [],
+    setCategoriesRefs: () => {}
 })
 
 export default MenuCategoriesRefsContext;
--- app/src/pages/menu/hooks/useCategoryRef.ts
+++ app/src/pages/menu/hooks/useCategoryRef.ts
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useRef } from "react";
 import { MenuCategoriesRefsContextProps } from "../menu_page.types";
 
-const useCategoryRef = (id: bigint, context: React.Context<MenuCategoriesRefsContextProps>) => {
-    const { setCategories } = useContext(context)
+const useCategoryRef = (id: string, context: React.Context<MenuCategoriesRefsContextProps>) => {
+    const { setCategoriesRefs: setCategories } = useContext(context)
 
     const categoryRef = useRef<HTMLDivElement>(null)
 
