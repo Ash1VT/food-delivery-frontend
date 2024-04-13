97daec4398c54d9f3bbcfe3cf8b350afa8c84525
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-13 20:02:44 +0300

    replaced with redux selector

--- app/src/pages/menu/MenuPage.tsx
+++ app/src/pages/menu/MenuPage.tsx
@@ -1,267 +1,25 @@
 import Navbar from 'src/components/navbar'
 import { FooterMenuColumnProps } from 'src/components/footer/footer.types'
 import Footer from 'src/components/footer'
-import MenuItem from './menu-item/MenuItem'
-import { MenuCategoriesRefsContextProps, MenuCategoryNameProps, MenuCategoryProps, MenuCategoryRef, MenuItemProps } from './menu_page.types'
-import MenuItemsList from './menu-items-list/MenuItemsList'
-import MenuCategory from './menu-category/MenuCategory'
+import { MenuCategoriesRefsContextProps, MenuCategoryNameProps, MenuCategoryProps, MenuCategoryRef } from './menu_page.types'
 import MenuCategoriesNames from './menu-categories-names/MenuCategoriesNames'
-import './menu_page.css'
 import Divider from 'src/components/ui/divider/Divider'
 import MenuCategoriesList from './menu-categories-list/MenuCategoriesList'
 import MenuCategoriesRefsContext from './contexts/MenuCategoriesRefsContext'
-import { useRef, useState } from 'react'
-import { ClickProps, ClicksContextProps } from 'src/hooks/useActiveClick'
-import MenuCategoriesNamesClicksContext from './contexts/MenuCategoriesNamesClicksContext'
+import { useState } from 'react'
 import OrderCart from 'src/components/order-cart/OrderCart'
-import { OrderCartItemProps } from 'src/components/order-cart/order_cart.types'
+import { useAppSelector } from 'src/hooks/redux/useAppSelector'
+import { getMenuCategories, getMenu } from './redux/selectors'
+import IMenuCategory from './redux/models/IMenuCategory'
+import RestaurantReference from 'src/components/restaurant-reference/RestaurantReference'
+import { getRestaurant } from '../restaurants/redux/selectors'
+import './menu_page.css'
 
 const MenuPage = () => {
 
-    const categoryNames: MenuCategoryNameProps[] = [
-        {
-            id: BigInt(1),
-            name: 'Pizzas',
-            imageUrl: 'images/pizza1.png',
-            isActive: true
-        },
-        {
-            id: BigInt(2),
-            name: 'Appetizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(3),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(4),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(5),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(6),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(7),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(8),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-        {
-            id: BigInt(9),
-            name: 'Vaizers',
-            imageUrl: 'images/pizza1.png',
-            isActive: false
-        },
-    ]
-
-    const menuCategories: MenuCategoryProps[] = [
-        {
-            id: BigInt(1),
-            name: 'Pizzas',
-            menuItems: [
-                {
-                    id: BigInt(1),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                },
-                {
-                    id: BigInt(1),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                },
-                {
-                    id: BigInt(1),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                },
-                {
-                    id: BigInt(1),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                },
-                {
-                    id: BigInt(1),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(2),
-            name: 'Appetizers',
-            menuItems: [
-                {
-                    id: BigInt(2),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(3),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(4),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(5),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(6),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(7),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(8),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-        {
-            id: BigInt(9),
-            name: 'Vaizers',
-            menuItems: [
-                {
-                    id: BigInt(3),
-                    imageUrl: 'images/food1.png',
-                    name: 'Hamburger',
-                    description: 'Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries Hamburger with cheese and fries',
-                    ratingValue: 4.5,
-                    reviewsCount: 10,
-                    price: 10,
-                    categoryId: BigInt(1)
-                }
-            ]
-        },
-  
-    ]
+    const { restaurantId, menuCategories } = useAppSelector(state => state.menuReducer.menu)
+    
+    const restaurant = useAppSelector(state => getRestaurant(state, restaurantId))
 
     const cities: string[] = [
         'San Francisco',
@@ -297,58 +55,38 @@ const MenuPage = () => {
         },
     ]
 
-    const orderCartItems: OrderCartItemProps[] = [
-        {
-            id: BigInt(1),
-            imageUrl: 'images/food1.png',
-            name: 'Hamburger',
-            categoryName: 'Pizzas',
-            price: 10,
-            quantity: 1
-        }
-    ]
-    
-    const [activeCategoryId, setActiveCategoryId] = useState<bigint>(BigInt(1))
-    const [clicks, setClicks] = useState<ClickProps[]>([])
-    const [categories, setCategories] = useState<MenuCategoryRef[]>([])
+    const [categoriesRefs, setCategoriesRefs] = useState<MenuCategoryRef[]>([])
 
 
     const menuCategoriesRefsContext: MenuCategoriesRefsContextProps = {
-        categories,
-        setCategories
+        categoriesRefs: categoriesRefs,
+        setCategoriesRefs: setCategoriesRefs
     }
 
-    const menuCategoryClicksContext: ClicksContextProps = {
-        clicks,
-        setClicks,
-        activeItemId: activeCategoryId,
-        setActiveItemId: setActiveCategoryId
-    }
 
     return (
         <div className="container menu__container">
             <Navbar/>
             <div className="menu__wrapper">
                     <MenuCategoriesRefsContext.Provider value={menuCategoriesRefsContext}>
-                        <MenuCategoriesNamesClicksContext.Provider value={menuCategoryClicksContext}>
                             <div className="menu__left__wrapper">
                                 {/* <div className="menu__back__to__restaurants__wrapper">
 
                                 </div> */}
-                                <MenuCategoriesNames categoryNames={categoryNames}/>
+                                <MenuCategoriesNames categoryNames={menuCategories}/>
                             </div>
-                            <Divider width='2px' height='auto' color='#CFCFCF' className='menu__divider'/>
+                            {/* <Divider width='2px' height='auto' color='#CFCFCF' className='menu__divider'/> */}
                             <div className="menu__content__wrapper">
 
                                 {/* <div className="menu__restaurant__wrapper">
 
                                 </div> */}
+                                <RestaurantReference {...restaurant}/>
                                 <MenuCategoriesList menuCategories={menuCategories}/>
                             </div>
                             <div className="menu__order__cart">
-                                <OrderCart items={orderCartItems}/>
+                                <OrderCart/>
                             </div>
-                        </MenuCategoriesNamesClicksContext.Provider>
                     </MenuCategoriesRefsContext.Provider>
             </div>
             <Footer cities={cities} cities_per_column={5} menu_columns={menu_columns} />
