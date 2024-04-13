8c5d94aa6e28f0d751e07f356d272659f57e5136
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-13 22:22:29 +0300

    changed page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import MainPage from './pages/main/MainPage';
 import MenuPage from './pages/menu/MenuPage';
 import ProfilePage from './pages/profile';
+import RestaurantReviewsPage from './pages/restaurant-reviews/RestaurantReviewsPage';
 import RestaurantsPage from './pages/restaurants/RestaurantsPage';
 import './styles/App.css';
 import './styles/buttons.css';
@@ -9,7 +10,7 @@ import '@smastrom/react-rating/style.css';
 
 const App = () => {
     return (
-        <MenuPage/>
+        <RestaurantReviewsPage/>
     )
 }
 
